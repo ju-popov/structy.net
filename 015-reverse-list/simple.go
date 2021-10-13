40a864a02f695414869caa81d4ebb261ea35e7be
Julian Popov <julian.popov@sumup.com>
2021-10-13 21:31:24 +0300

    Problem: reverse list

--- 015-reverse-list/simple.go
+++ 015-reverse-list/simple.go
@@ -1,12 +1,9 @@
 package reverselist
 
 func Simple(head *Node) *Node {
-	var (
-		prev    *Node
-		current *Node
-	)
+	var prev *Node
 
-	for current = head; current != nil; {
+	for current := head; current != nil; {
 		next := current.Next
 		current.Next = prev
 		prev = current
