bfc6a7b353459bd506183d93f05df0d18e37fbd5
Julian Popov <julian.popov@sumup.com>
2021-10-13 18:59:09 +0300

    Problem: five sort

--- 010-five-sort/simple.go
+++ 010-five-sort/simple.go
@@ -10,13 +10,13 @@ func find(nums []int64, minIndex int, maxIndex int, value int64) int {
 	return -1
 }
 
-func Simple(nums []int64) []int64 {
-	targetValue := int64(5)
+const targetValue = int64(5)
 
+func Simple(nums []int64) []int64 {
 	indexLeft := -1
 	for indexRight := len(nums) - 1; indexLeft < indexRight; indexRight-- {
-		if nums[indexRight] != 5 {
-			if targetIndex := find(nums, indexLeft + 1, indexRight - 1, targetValue); targetIndex != -1 {
+		if nums[indexRight] != targetValue {
+			if targetIndex := find(nums, indexLeft+1, indexRight-1, targetValue); targetIndex != -1 {
 				nums[targetIndex] = nums[indexRight]
 				nums[indexRight] = targetValue
 				indexLeft = targetIndex
