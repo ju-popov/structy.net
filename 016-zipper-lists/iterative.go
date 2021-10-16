fe10eba6106aaa7bdc1616fe23489e5c4e6b79af
Julian Popov <julian.popov@sumup.com>
2021-10-17 01:36:26 +0300

    Problem: zipper lists

--- 016-zipper-lists/iterative.go
+++ 016-zipper-lists/iterative.go
@@ -7,37 +7,25 @@ func Iterative(head1 *Node, head2 *Node) *Node {
 	)
 
 	for (head1 != nil) || (head2 != nil) {
-		if head1 != nil {
-			if first == nil {
-				first = head1
-			}
-
-			if last != nil {
-				last.Next = head1
-			}
-
-			last = head1
-
-			next := last.Next
-			last.Next = nil
-			head1 = next
+		if head1 == nil {
+			head1, head2 = head2, head1
 		}
 
-		if head2 != nil {
-			if first == nil {
-				first = head2
-			}
-
-			if last != nil {
-				last.Next = head2
-			}
-
-			last = head2
-
-			next := last.Next
-			last.Next = nil
-			head2 = next
+		if first == nil {
+			first = head1
 		}
+
+		if last != nil {
+			last.Next = head1
+		}
+
+		last = head1
+
+		next := last.Next
+		last.Next = nil
+		head1 = next
+
+		head1, head2 = head2, head1
 	}
 
 	return first
