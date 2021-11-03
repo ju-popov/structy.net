1af4cdbcc44ad79ccd06a97cb99e414ef8feb5ed
Julian Popov <julian.popov@sumup.com>
2021-11-03 06:59:47 +0200

    Problem: counting change

--- 059-counting-change/recursive.go
+++ 059-counting-change/recursive.go
@@ -6,25 +6,25 @@ type memoryKey struct {
 }
 
 func helper(amount int, coins []int, coinIndex int, memory map[memoryKey]int) int {
-	if coinIndex >= len(coins) {
-		return 0
-	}
-
 	key := memoryKey{amount: amount, coinIndex: coinIndex}
 	if value, ok := memory[key]; ok {
 		return value
 	}
 
+	if amount == 0 {
+		return 1
+	}
+
+	if coinIndex >= len(coins) {
+		return 0
+	}
+
 	count := 0
 	coin := coins[coinIndex]
 
-	for quantity := 0; quantity*coin <= amount; quantity++ {
-		remainder := amount - quantity*coin
-		if remainder == 0 {
-			count++
-		} else {
-			count += helper(remainder, coins, coinIndex+1, memory)
-		}
+	for qty := 0; (qty * coin) <= amount; qty++ {
+		remainder := amount - qty*coin
+		count += helper(remainder, coins, coinIndex+1, memory)
 	}
 
 	memory[key] = count
