589074b756b58060f3f6f4b45f579c34795d09c7
Julian Popov <julian.popov@sumup.com>
2021-11-03 19:08:02 +0200

    Problem: shortest path

--- 042-shortest-path/breadth_first.go
+++ 042-shortest-path/breadth_first.go
@@ -1,5 +1,10 @@
 package shortestpath
 
+type queueValue struct {
+	node     string
+	distance int
+}
+
 func createGraph(edges [][2]string) map[string][]string {
 	graph := make(map[string][]string)
 
@@ -11,35 +16,31 @@ func createGraph(edges [][2]string) map[string][]string {
 	return graph
 }
 
-type nodeInfo struct {
-	name     string
-	distance int
-}
-
 func BreadthFirst(edges [][2]string, nodeA string, nodeB string) int {
 	graph := createGraph(edges)
 	visited := make(map[string]bool)
 
-	queue := []nodeInfo{
+	queue := []queueValue{
 		{
-			name:     nodeA,
+			node:     nodeA,
 			distance: 0,
 		},
 	}
 
 	for len(queue) > 0 {
-		node := queue[0]
+		node := queue[0].node
+		distance := queue[0].distance
 		queue = queue[1:]
 
-		if node.name == nodeB {
-			return node.distance
+		if node == nodeB {
+			return distance
 		}
 
-		visited[node.name] = true
+		visited[node] = true
 
-		for _, next := range graph[node.name] {
-			if !visited[next] {
-				queue = append(queue, nodeInfo{name: next, distance: node.distance + 1})
+		for _, neighbor := range graph[node] {
+			if !visited[neighbor] {
+				queue = append(queue, queueValue{node: neighbor, distance: distance + 1})
 			}
 		}
 	}
